688bfc38d68569e4a4388e53aa0448315e6b7eec
saqhan <saqhan1@mail.ru>
2020-01-15 15:06:20 +0300

    40

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -24,7 +24,7 @@ export default class TodoListItem extends Component {
     };
 
     render() {    
-        const { label } = this.props;
+        const { label, onDeleted } = this.props;
         const { done, important } = this.state;
 
         let classNames = 'todo-list-item';
@@ -52,7 +52,8 @@ export default class TodoListItem extends Component {
                 </button>
             
                 <button type="button"
-                    className="btn btn-outline-danger btn-sm float-right">
+                    className="btn btn-outline-danger btn-sm float-right"
+                    onClick= { onDeleted}>
                     <i className="fa fa-trash-o" />
                 </button>
 
